e24b856a42352cfdc7addfbf97610524985c217d
Joel Kleier <joel.kleier@wildcardcorp.com>
2023-01-12 15:08:23 -0600

    back to dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
     ]
 
 setup(name='princexmlserver',
-      version='0.2.0',
+      version='0.2.1.dev0',
       description='princexmlserver',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
