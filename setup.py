9c36f2bebc2d14c9ccd73873e9fbac7cd89affe8
Joel Kleier <joel.kleier@wildcardcorp.com>
2023-01-25 07:37:30 -0600

    Back to development: 0.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
     ]
 
 setup(name='princexmlserver',
-      version='0.3.1',
+      version='0.3.2.dev0',
       description='princexmlserver',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
